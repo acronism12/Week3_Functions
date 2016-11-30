4ddb3843016b881ea10847e1c1d214707968fa7c
Acronism <alan.barrie@acronism.co.uk>
2016-11-30 20:04:42 +0000

    Added comments

--- addfunction.rb
+++ addfunction.rb
@@ -1,14 +1,15 @@
-# function to add two parameters
-
+# function to add two numbers
 def addfunction(a, b)
   add_result = a.to_i() + b.to_i()
   return add_result
 end
 
+# get user input
 print "Enter first number to add:"
 num_1 = gets.chomp
 print "Enter second number to add:"
 num_2 = gets.chomp
 
+#invoke function and output result
 result = addfunction(num_1,num_2)
 puts "The sum of the two numbers is #{result}"
--- populationdensity.rb
+++ populationdensity.rb
@@ -1,13 +1,15 @@
-
+#population density function
 def popdensity(population, area)
   divide_result = population.to_i() / area.to_i()
   return divide_result
 end
 
+# request input from user
 print "What is the population? "
 population_string = gets.chomp
 print "What is the area? "
 area_string = gets.chomp
 
+# invoke function and output result
 result = popdensity(population_string, area_string)
 puts "Population density = #{result}"
