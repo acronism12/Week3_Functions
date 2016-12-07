efc8af011163ca1097cc9ce56f08b9c55df01ea0
Acronism <alan.barrie@acronism.co.uk>
2016-12-07 19:09:28 +0000

    Updated file with comments in class

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -1,34 +1,35 @@
-def add(num_1,num_2)
-  add_result = num_1 + num_2
-  return add_result
+def add(num_1, num_2)
+  result = num_1 + num_2
+  return result
 end
 
-def subtract(num_1,num_2)
-  subtract_result = num_1 - num_2
-  return subtract_result
+def subtract(num_1, num_2)
+  result = num_1 - num_2
+  return result
 end
 
-def multiply(num_1,num_2)
-  multiply_result = num_1 * num_2
-  return multiply_result
+def multiply(num_1, num_2)
+  result = num_1 * num_2
+  return result
 end
 
-def divide(num_1,num_2)
-  divide_result = num_1 / num_2
-  return divide_result
+def divide(num_1, num_2)
+  result = num_1 / num_2
+  return result
 end
 
 def length_of_string(input_string)
-  length_of_string = input_string.length
+  length_of_string = input_string.length #could also use .size
   return length_of_string
 end
 
-def join_string(string_1,string_2)
-  joined_string = string_1 + string_2
+def join_string(string_1, string_2)
+  joined_string = string_1 + string_2 #string concatenation - best method
+  #joined_string = "#{string_1}#{string_2}" #string interpolation - alternative
   return joined_string
 end
 
-def add_string_as_number(string_1,string_2)
-  add_result = string_1.to_i() + string_2.to_i()
-  return add_result
+def add_string_as_number(string_1, string_2)
+  result = string_1.to_i() + string_2.to_i()
+  return result
 end
